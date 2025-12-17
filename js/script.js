c03829209ce77e93c25a4134e02868dd8d5c5c33
AsadLxj <asadullaxujaulmasov@gmail.com>
2025-12-17 18:13:38 +0500

    new

--- js/script.js
+++ js/script.js
@@ -44,6 +44,18 @@ function renderMesseng(arr, list) {
             `
         
         }
+        if (item.audio) {
+            elItem.innerHTML = `
+            <li class="bg-[#0088cc] ml-auto p-2 rounded-[18px] text-white w-[80%]">
+                <audio controls class="w-full">
+                    <source src="${item.audio}" type="audio/webm">
+                </audio>
+                <div class="text-end text-[12px]">
+                    <span>${item.createAt}</span>
+                </div>
+            </li>
+            `
+        }
         list.appendChild(elItem)
     })
 }
@@ -79,3 +91,46 @@ elMessegForm.addEventListener("submit", (evt) => {
     evt.target.reset()
 })
 // Submit message end
+let micBtn = document.querySelector("#micBtn")
+let mediaRecorder
+let audioChunks = []
+
+
+micBtn.addEventListener("click", async () => {
+    if (!mediaRecorder || mediaRecorder.state === "inactive") {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+        mediaRecorder = new MediaRecorder(stream)
+        audioChunks = []
+
+        mediaRecorder.ondataavailable = e => {
+            audioChunks.push(e.data)
+        }
+
+        mediaRecorder.onstop = () => {
+            const audioBlob = new Blob(audioChunks, { type: "audio/webm" })
+            const audioUrl = URL.createObjectURL(audioBlob)
+
+            let date = new Date()
+            let time = `${date.getHours()}:${date.getMinutes()}`
+
+            const data = {
+                id: messageList.length ? messageList.at(-1).id + 1 : 1,
+                audio: audioUrl,
+                content: "",
+                createAt: time
+            }
+
+            messageList.push(data)
+            set("message", messageList)
+            renderMesseng(messageList, elList)
+        }
+
+        mediaRecorder.start()
+        micBtn.classList.add("opacity-50")
+    } else {
+        mediaRecorder.stop()
+        micBtn.classList.remove("opacity-50")
+    }
+})
+
+
